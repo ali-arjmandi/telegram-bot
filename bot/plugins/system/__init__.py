1771c7a11a8deff529bdc654331ba576b1e7457d
ali-arjmandi <aliarjmandi339@gmail.com>
2022-12-25 15:53:03 +0330

    feat: :sparkles: get user info

--- bot/plugins/system/__init__.py
+++ bot/plugins/system/__init__.py
@@ -1,10 +1,12 @@
 from pyrogram import filters
-from .task import hi, upTime, help
+from .task import hi, upTime, help, info
 
 handlers = [
     (hi, filters.me & filters.text & filters.command('hi'), 'say hi'),
     (upTime, filters.me & filters.text &
      filters.command('uptime'), 'get upTime'),
+    (info, filters.reply & filters.outgoing & filters.text &
+     filters.command('info'), 'get user\'s info'),
     (help, filters.me & filters.text &
      filters.command('help'), 'get documentation'),
 ]
--- bot/plugins/system/task.py
+++ bot/plugins/system/task.py
@@ -1,4 +1,5 @@
 from pyrogram import types
+from pyrogram.client import Client
 from datetime import datetime
 from ...help import get as getHelp
 time = datetime.now()
@@ -14,6 +15,15 @@ async def upTime(_, message: types.Message):
     hours, remainder = divmod(s, 3600)
     minutes, seconds = divmod(remainder, 60)
     await message.reply('{:02}:{:02}:{:02}'.format(int(hours), int(minutes), int(seconds)))
+
+
+async def info(client: Client, message: types.Message):
+    replied_user = message.reply_to_message.from_user
+    id = replied_user.id
+    full_name = f'{replied_user.first_name} {replied_user.last_name}'
+    username = replied_user.username
+    await client.send_message("me", f'ID: {id}\nname: {full_name}\nusername: @{username}',)
+    await message.delete()
 
 
 async def help(_, message: types.Message):
